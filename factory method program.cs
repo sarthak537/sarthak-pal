ecd30c139ea5b12a940c8dbf706066d326e5ba80
sarthak537 <71943119+sarthak537@users.noreply.github.com>
2022-09-01 01:23:51 -0700

    Add files via upload

---
+++ factory method program.cs
@@ -0,0 +1,28 @@
+﻿class Test
+{
+    private Test() { }
+    private static Test test;
+
+    public void show()
+    {
+        System.Console.WriteLine("hello " + this.GetHashCode());
+    }
+    public static Test GetTestobject()
+    {
+        if (test == null)
+        {
+            test = new Test();
+        }
+        return test;
+    }
+}
+class Demo
+{
+    static void Main1()
+    {
+        Test t = Test.GetTestobject();
+        t.show();
+        Test t1 = Test.GetTestobject();
+        t1.show();
+    }
+}
